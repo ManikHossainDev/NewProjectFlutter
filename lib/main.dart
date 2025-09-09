cc7b0cf865494ad48a1ac8749437e1cbaef7cbf2
ManikHossainDev <se.manik.js@gmail.com>
2025-09-09 11:43:04 +0600

    What are Expanded Widgets in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class DashBordScreen extends StatelessWidget {
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("DashBord Screen"),
+        title: Text(
+          "DashBord Screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Text("Welcome to DashBord Screen"),
+      body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+           Container(width: 100, height: 100, color: Colors.blue.  shade50),
+           Expanded(
+            child:Container(width: 100, height: 100, color: Colors.blue.  shade100),
+           ), 
+           Container(width: 100, height: 100, color: Colors.yellow.shade500,)
+          ],
       ),
     );
   }
---
+++ lib/text.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Mypp());
+}
+
+class Mypp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Md Manik Hossain",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DashBordScreen(),
+    );
+  }
+}
+
+class DashBordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DashBord Screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blue.shade50,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              shape: BoxShape.circle,
+              //  borderRadius: BorderRadius.circular(21),
+              // borderRadius: BorderRadius.only(
+              //   topLeft: Radius.circular(21),
+              //   bottomRight: Radius.circular(21),
+              // ),
+              border: Border.all(color: Colors.blue, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  spreadRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

6f1317d10649878a089c1c99ad1083ce299b1464
ManikHossainDev <se.manik.js@gmail.com>
2025-10-23 18:09:01 +0600

    update

--- lib/BMIAPP.dart
+++ lib/BMIAPP.dart
@@ -1,10 +1,8 @@
 
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -24,12 +22,12 @@ class BMIAPP extends StatefulWidget {
   BMIAPPState createState() => BMIAPPState();
 }
 
-
 class BMIAPPState extends State<BMIAPP> {
-
   var wtController = TextEditingController();
   var ftController = TextEditingController();
   var inController = TextEditingController();
+
+  var result = "";
 
   @override
   Widget build(BuildContext context) {
@@ -46,48 +44,81 @@ class BMIAPPState extends State<BMIAPP> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 11),
-          Text("BMI", style: TextStyle(
-            fontSize: 34,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue.shade400
-          ),),
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 11),
+              Text("BMI", style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue.shade400
+              ),),
 
-          SizedBox(height: 11),
-          TextField(
-            controller:wtController ,
-            decoration:  InputDecoration(
-              label: Text("Enter Your Weight(in kgs)"),
-              prefixIcon: Icon(Icons.line_weight)
-            ),
-            keyboardType: TextInputType.number,
+              SizedBox(height: 11),
+              TextField(
+                controller:wtController ,
+                decoration:  InputDecoration(
+                  label: Text("Enter Your Weight(in kgs)"),
+                  prefixIcon: Icon(Icons.line_weight)
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+              SizedBox(height: 11),
+
+              TextField(
+                controller:ftController ,
+                decoration:  InputDecoration(
+                    label: Text("Enter Your Weight(in)"),
+                    prefixIcon: Icon(Icons.line_style)
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+              SizedBox(height: 11),
+
+              TextFormField(
+                 controller: inController,
+                decoration: InputDecoration(
+                  label: Text("Enter Your Weight (in)"),
+                  prefixIcon: Icon(Icons.blur_linear),
+              ),
+                  keyboardType: TextInputType.number
+              ),
+
+              SizedBox(height: 18),
+
+              ElevatedButton(onPressed: (){
+
+                var wt = wtController.text.toString();
+                var ft=ftController.text.toString();
+                var inch = inController.text.toString();
+
+                 if(wt!="" && ft!="" && inch!=""){
+                   // calculat
+                 }else {
+                    setState(() {
+                      result = "please fill all the required blanks!!";
+                    });
+                 }
+              },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 5, // shadow
+                  ),
+                  child: Text("Calculator")),
+                Text(result, style: TextStyle(fontSize: 34, fontWeight: FontWeight.w600, color: Colors.green),)
+            ],
           ),
-
-          SizedBox(height: 11),
-
-          TextField(
-            controller:ftController ,
-            decoration:  InputDecoration(
-                label: Text("Enter Your Weight(in)"),
-                prefixIcon: Icon(Icons.line_style)
-            ),
-            keyboardType: TextInputType.number,
-          ),
-
-          SizedBox(height: 11),
-
-          TextField(
-             controller: inController,
-            decoration: InputDecoration(
-              label: Text("Enter Your Weight (in)"),
-              prefixIcon: Icon(Icons.blur_linear),
-          ),
-              keyboardType: TextInputType.number
-          )
-
-        ],
+        ),
       )
     );
   }

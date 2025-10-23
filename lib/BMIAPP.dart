0729106816d76fb7201d85dce18d6285305ab900
Programmer-Manik <programmermanik1502083874@gmail.com>
2025-10-23 23:14:48 +0600

    uild BMI APP in Flutter  Create

--- lib/BMIAPP.dart
+++ lib/BMIAPP.dart
@@ -28,6 +28,7 @@ class BMIAPPState extends State<BMIAPP> {
   var inController = TextEditingController();
 
   var result = "";
+  var BgColore = Colors.indigo.shade200 ;
 
   @override
   Widget build(BuildContext context) {
@@ -44,79 +45,118 @@ class BMIAPPState extends State<BMIAPP> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 11),
-              Text("BMI", style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue.shade400
-              ),),
+      body: Container(
+         color: BgColore,
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 11),
+                Text("BMI", style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue.shade400
+                ),),
 
-              SizedBox(height: 11),
-              TextField(
-                controller:wtController ,
-                decoration:  InputDecoration(
-                  label: Text("Enter Your Weight(in kgs)"),
-                  prefixIcon: Icon(Icons.line_weight)
-                ),
-                keyboardType: TextInputType.number,
-              ),
-
-              SizedBox(height: 11),
-
-              TextField(
-                controller:ftController ,
-                decoration:  InputDecoration(
-                    label: Text("Enter Your Weight(in)"),
-                    prefixIcon: Icon(Icons.line_style)
-                ),
-                keyboardType: TextInputType.number,
-              ),
-
-              SizedBox(height: 11),
-
-              TextFormField(
-                 controller: inController,
-                decoration: InputDecoration(
-                  label: Text("Enter Your Weight (in)"),
-                  prefixIcon: Icon(Icons.blur_linear),
-              ),
-                  keyboardType: TextInputType.number
-              ),
-
-              SizedBox(height: 18),
-
-              ElevatedButton(onPressed: (){
-
-                var wt = wtController.text.toString();
-                var ft=ftController.text.toString();
-                var inch = inController.text.toString();
-
-                 if(wt!="" && ft!="" && inch!=""){
-                   // calculat
-                 }else {
-                    setState(() {
-                      result = "please fill all the required blanks!!";
-                    });
-                 }
-              },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    elevation: 5, // shadow
+                SizedBox(height: 11),
+                TextField(
+                  controller:wtController ,
+                  decoration:  InputDecoration(
+                    label: Text("Enter Your Weight(in kgs)"),
+                    prefixIcon: Icon(Icons.line_weight)
                   ),
-                  child: Text("Calculator")),
-                Text(result, style: TextStyle(fontSize: 34, fontWeight: FontWeight.w600, color: Colors.green),)
-            ],
+                  keyboardType: TextInputType.number,
+                ),
+
+                SizedBox(height: 11),
+
+                TextField(
+                  controller:ftController ,
+                  decoration:  InputDecoration(
+                      label: Text("Enter Your Weight(in)"),
+                      prefixIcon: Icon(Icons.line_style)
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+
+                SizedBox(height: 11),
+
+                TextFormField(
+                   controller: inController,
+                  decoration: InputDecoration(
+                    label: Text("Enter Your Weight (in)"),
+                    prefixIcon: Icon(Icons.blur_linear),
+                ),
+                    keyboardType: TextInputType.number
+                ),
+
+                SizedBox(height: 18),
+
+                ElevatedButton(onPressed: (){
+
+                  var wt = wtController.text.toString();
+                  var ft=ftController.text.toString();
+                  var inch = inController.text.toString();
+
+                   if(wt!="" && ft!="" && inch!=""){
+                     // calculat
+                     var twt = int.parse(wt);
+                     var tft = int.parse(ft);
+                     var iInch = int.parse(inch);
+
+                     var tInch = (tft * 10) + iInch ;
+
+                     var tCm = tInch * 2.54;
+
+                     var tM = tCm/100;
+
+                     var bmi = twt/(tM*tM);
+
+
+                     var msg = "";
+
+
+
+                     if(bmi > 25 ){
+                        msg = "You're OverWeight!!";
+                        BgColore = Colors.orange.shade200;
+                     } else if (bmi < 18) {
+                       msg = "You're UnderWeight!!!";
+                       BgColore = Colors.red.shade200;
+                     } else  {
+                        msg = "You're Healthy!!!";
+                        BgColore = Colors.green.shade200;
+                     }
+
+
+                     setState(() {
+                       result =  "$msg \n yor BMI is : ${bmi.toStringAsFixed(2)}";
+                     });
+
+                   }else {
+                      setState(() {
+                        result = "please fill all the required blanks!!";
+                      });
+                   }
+                },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 5, // shadow
+                    ),
+                    child: Text("Calculator")),
+
+                   SizedBox(height: 18),
+
+                  Text(result, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.green),)
+              ],
+            ),
           ),
         ),
       )
